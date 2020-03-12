94339634fdc90e8f92f4ab5bafbaaf76f9f6ef5f
Timothy Purchas <timothy.purchas@iag.com.au>
2020-03-13 10:13:07 +1100

    add cosmos extension tests

--- packages/@cdk-cosmos/core/test/cosmos.test.ts
+++ packages/@cdk-cosmos/core/test/cosmos.test.ts
@@ -2,40 +2,67 @@ import '@aws-cdk/assert/jest';
 import { App } from '@aws-cdk/core';
 import { SynthUtils } from '@aws-cdk/assert';
 import { CloudFormationStackArtifact } from '@aws-cdk/cx-api';
-import { CosmosStack } from '../src';
+import { CosmosStack, CosmosExtensionStack } from '../src';
 
 const toHaveResourceId = (stack: CloudFormationStackArtifact, id: string): void => {
   expect(stack.template.Resources).toHaveProperty(id);
 };
 
+const toHaveResourceCount = (stack: CloudFormationStackArtifact, length: number): void => {
+  expect(Object.keys(stack.template.Resources)).toHaveLength(length);
+};
+
 const app = new App();
 const cosmos = new CosmosStack(app, 'Test', { tld: 'com' });
-const stack = SynthUtils.synthesize(cosmos);
+const cosmosExtension = new CosmosExtensionStack(app, 'Test');
+const cosmosStack = SynthUtils.synthesize(cosmos);
+const cosmosExtensionStack = SynthUtils.synthesize(cosmosExtension);
 
 describe('Cosmos', () => {
   test('should be a cosmos', () => {
-    expect(stack.name).toEqual('Core-Test-Cosmos');
-    expect(stack).toHaveOutput({ exportName: 'Core-Name', outputValue: 'Test' });
-    expect(stack).toHaveOutput({ exportName: 'Core-Version' });
-    expect(Object.keys(stack.template.Resources)).toHaveLength(3);
-    expect(stack.template).toMatchSnapshot();
+    expect(cosmosStack.name).toEqual('Core-Test-Cosmos');
+    expect(cosmosStack).toHaveOutput({ exportName: 'Core-Name', outputValue: 'Test' });
+    expect(cosmosStack).toHaveOutput({ exportName: 'Core-Version' });
+    toHaveResourceCount(cosmosStack, 3);
   });
 
   test('should have a RootZone', () => {
-    expect(stack).toHaveResource('AWS::Route53::HostedZone', { Name: 'test.com.' });
-    expect(stack).toHaveOutput({ exportName: 'Core-RootZone-Id' });
-    expect(stack).toHaveOutput({ exportName: 'Core-RootZone-Name' });
-    toHaveResourceId(stack, 'RootZone831A5F27');
+    expect(cosmosStack).toHaveResource('AWS::Route53::HostedZone', { Name: 'test.com.' });
+    expect(cosmosStack).toHaveOutput({ exportName: 'Core-RootZone-Id' });
+    expect(cosmosStack).toHaveOutput({ exportName: 'Core-RootZone-Name' });
+    toHaveResourceId(cosmosStack, 'RootZone831A5F27');
   });
 
   test('should have a CdkRepo', () => {
-    expect(stack).toHaveResource('AWS::CodeCommit::Repository', { RepositoryName: 'core-cdk-repo' });
-    expect(stack).toHaveOutput({ exportName: 'Core-CdkRepo-Name' });
-    toHaveResourceId(stack, 'CdkRepo9606A710');
+    expect(cosmosStack).toHaveResource('AWS::CodeCommit::Repository', { RepositoryName: 'core-cdk-repo' });
+    expect(cosmosStack).toHaveOutput({ exportName: 'Core-CdkRepo-Name' });
+    toHaveResourceId(cosmosStack, 'CdkRepo9606A710');
   });
 
   test('should have a CdkMasterRole', () => {
-    expect(stack).toHaveResource('AWS::IAM::Role', { RoleName: 'Core-CdkMaster-Role' });
-    toHaveResourceId(stack, 'CdkMasterRole2FE9B317');
+    expect(cosmosStack).toHaveResource('AWS::IAM::Role', { RoleName: 'Core-CdkMaster-Role' });
+    toHaveResourceId(cosmosStack, 'CdkMasterRole2FE9B317');
+  });
+
+  test('should match snapshot', () => {
+    expect(cosmosStack.template).toMatchSnapshot();
+  });
+});
+
+describe('Cosmos Extension', () => {
+  test('should be a cosmos extension', () => {
+    expect(cosmosExtensionStack.name).toEqual('App-Test-Cosmos');
+    toHaveResourceCount(cosmosExtensionStack, 1);
+  });
+
+  test('should have a CdkRepo', () => {
+    expect(cosmosExtensionStack).toHaveResource('AWS::CodeCommit::Repository', { RepositoryName: 'app-test-cdk-repo' });
+    toHaveResourceId(cosmosExtensionStack, 'CdkRepo9606A710');
+  });
+
+  //TODO: Check if imports align
+
+  test('should match snapshot', () => {
+    expect(cosmosExtensionStack.template).toMatchSnapshot();
   });
 });
