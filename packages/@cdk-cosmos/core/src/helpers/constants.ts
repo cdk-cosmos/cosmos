5acc819b8e9eb61fbe6cbcdc5af22898a266ecf4
Timothy Purchas <timothy.purchas@iag.com.au>
2020-05-28 12:58:06 +1000

    clean up

--- packages/@cdk-cosmos/core/src/helpers/constants.ts
+++ packages/@cdk-cosmos/core/src/helpers/constants.ts
@@ -1,3 +1,9 @@
+export const AWS_ENV = 'AWS_ENV';
+
+export const COSMOS_PARTITION = 'COSMOS_PARTITION';
+export const COSMOS_VERSION = 'COSMOS_VERSION';
+export const COSMOS_NETWORK_BUILDER = 'COSMOS_NETWORK_BUILDER';
+
 export const PATTERN = {
   SINGLETON_COSMOS: '{Partition}{Resource}+',
   SINGLETON_GALAXY: '{Partition}{Galaxy}{Resource}+',
@@ -5,13 +11,4 @@ export const PATTERN = {
   STACK: '{Partition}{Cosmos}{Galaxy}?{SolarSystem}?{Version}?{Type}',
   COSMOS: '{Partition}{Cosmos}{Galaxy}?{SolarSystem}?{Resource}*{Version}?',
   RESOURCE: '{Resource}+{Version}?',
-  // SHORT_SOLAR_SYSTEM: '{Partition}{Cosmos}{SolarSystem}{Resource}+',
-  // DOCKER_TAG: '{Cosmos}/{Resource}+',
-  // LOG_GROUP: '{Partition}/{Cosmos}/{SolarSystem}/{Resource}+',
 };
-
-export const COSMOS_PARTITION = 'COSMOS_PARTITION';
-export const COSMOS_VERSION = 'COSMOS_VERSION';
-
-export const COSMOS_NETWORK_BUILDER = 'COSMOS_NETWORK_BUILDER';
-export const AWS_ENV = 'AWS_ENV';
