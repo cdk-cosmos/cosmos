ca26d7c1130dac30bda32a8b4541b5459d951d63
Japan Naik <Japan.Naik@iag.com.au>
2020-06-24 13:39:43 +1000

    Add SecureBucket building block

---
+++ packages/@cosmos-building-blocks/s3/src/index.ts
@@ -0,0 +1,5 @@
+// export {
+
+// } from './app-alb-nlb-combo';
+
+export * from './s3';
---
+++ packages/@cosmos-building-blocks/s3/src/s3.ts
@@ -0,0 +1,28 @@
+import { Construct } from '@aws-cdk/core';
+import { Bucket, BucketEncryption, BucketProps } from '@aws-cdk/aws-s3';
+import { PolicyStatement, Effect, AccountPrincipal } from '@aws-cdk/aws-iam';
+
+export interface SecureBucketProps extends BucketProps {
+  prefix?: string;
+}
+
+export class SecureBucket extends Construct {
+  readonly bucket: Bucket;
+
+  constructor(scope: Construct, id: string, props?: SecureBucketProps) {
+    super(scope, id);
+    const bucket = new Bucket(this, id, {
+      encryption: BucketEncryption.S3_MANAGED,
+      serverAccessLogsPrefix: 'BucketAccessLogs',
+      ...props,
+    });
+    const policyStatement = new PolicyStatement({
+      actions: ['s3:*'],
+      principals: [new AccountPrincipal('*')],
+      effect: Effect.DENY,
+      sid: 'ForceSSLOnly',
+    });
+    policyStatement.addCondition('Bool', { 'aws:SecureTransport': false });
+    bucket.addToResourcePolicy(policyStatement);
+  }
+}
