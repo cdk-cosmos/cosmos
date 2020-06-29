35958f96a2bcfa607cfbb868a9e268c9aa2e3c9f
Timothy Purchas <timothy.purchas@iag.com.au>
2020-06-29 16:25:42 +1000

    add changelog

--- scripts/version.sh
+++ scripts/version.sh
@@ -3,12 +3,22 @@ prevous_version=$(git describe --abbrev=0 --tags)
 version_bump=${VERSION_BUMP:-"patch"}
 push=${PUSH:-"false"}
 
+if [ !$GITHUB_AUTH ]; then
+    echo "Missing GITHUB_AUTH env"
+fi
+
 echo $prevous_version, $version_bump, $push
 
-exec yarn lerna version $version_bump --force-publish=* --yes --no-push
+yarn lerna version $version_bump --force-publish=* --yes --no-push
 
 curent_version=$(git describe --abbrev=0 --tags)
 
 echo $prevous_version, $curent_version, $version_bump, $push
 
-echo -e "$(yarn -s lerna-changelog --from=$prevous_version --to $curent_version)\n$(cat changelog.md)" > changelog.md
+echo "$(yarn -s lerna-changelog --from=$prevous_version --to $curent_version)\n$(cat changelog.md)" > changelog.md
+
+git add . && git commit --amend --no-edit
+
+if [ $push == "true" ]; then
+    git push
+fi
