36b395227011bff843d1f64ca5ee22549a75fbf9
jbnaik <japan.naik29@gmail.com>
2020-07-01 09:55:11 +1000

    Add SecureBucket building block (#99)
    * Add SecureBucket building block
    
    * update principal in policy to any
    
    * move SecureBucket into buildingblock-service
    
    * fix policy to restrict only to the bucket objects
    
    * fix SecureBucket property
    
    * fix accessControl and extend from Bucket
    
    * Change all buckets to SecureBuckets
    
    Co-authored-by: Japan Naik <Japan.Naik@iag.com.au>

---
+++ packages/@cosmos-building-blocks/service/test/s3.test.ts
@@ -0,0 +1,14 @@
+import { expect as expectCDK, haveResource, SynthUtils } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import * as s3 from '../src/index';
+
+test('Empty Stack', () => {
+  const app = new cdk.App();
+  const stack = new cdk.Stack(app, 'aws-secure-bucket');
+
+  //WHEN
+  // new s3.SecureBucket(stack, 'SecureBucket', { serverAccessLogsPrefix: '' });
+  new s3.SecureBucket(stack, 'SecureBucket', {});
+  // THEN
+  expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
+});
