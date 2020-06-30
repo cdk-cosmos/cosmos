2c576e9d8a808eb1720537f6c79f94278ddfea23
Japan Naik <Japan.Naik@iag.com.au>
2020-06-30 14:29:23 +1000

    fix accessControl and extend from Bucket

--- packages/@cosmos-building-blocks/service/src/s3.ts
+++ packages/@cosmos-building-blocks/service/src/s3.ts
@@ -1,25 +1,23 @@
 import { Construct } from '@aws-cdk/core';
-import { Bucket, BucketEncryption, BucketProps } from '@aws-cdk/aws-s3';
+import { Bucket, BucketEncryption, BucketProps, BucketAccessControl } from '@aws-cdk/aws-s3';
 import { PolicyStatement, Effect, AnyPrincipal } from '@aws-cdk/aws-iam';
 
-export class SecureBucket extends Construct {
-  readonly bucket: Bucket;
-
+export class SecureBucket extends Bucket {
   constructor(scope: Construct, id: string, props?: BucketProps) {
-    super(scope, id);
-    this.bucket = new Bucket(this, id, {
+    super(scope, id, {
       encryption: BucketEncryption.S3_MANAGED,
-      serverAccessLogsPrefix: 'BucketAccessLogs',
+      accessControl: BucketAccessControl.LOG_DELIVERY_WRITE,
+      serverAccessLogsPrefix: 'BucketLogs/',
       ...props,
     });
     const policyStatement = new PolicyStatement({
       actions: ['s3:*'],
       principals: [new AnyPrincipal()],
-      resources: [`${this.bucket.bucketArn}/*`],
+      resources: [`${this.bucketArn}/*`],
       effect: Effect.DENY,
       sid: 'ForceSSLOnly',
     });
     policyStatement.addCondition('Bool', { 'aws:SecureTransport': false });
-    this.bucket.addToResourcePolicy(policyStatement);
+    super.addToResourcePolicy(policyStatement);
   }
 }
