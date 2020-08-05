a1dea907ebc6c19683312ae2c9354f7ae7e8f63a
Timothy Purchas <timothy.purchas@iag.com.au>
2020-08-05 11:16:07 +1000

    add tests

--- packages/@cosmos-building-blocks/service/test/ec2-cloud-watch-agent.test.ts
+++ packages/@cosmos-building-blocks/service/test/ec2-cloud-watch-agent.test.ts
@@ -3,7 +3,7 @@ import { SynthUtils } from '@aws-cdk/assert';
 import { App, Stack } from '@aws-cdk/core';
 import { AutoScalingGroup } from '@aws-cdk/aws-autoscaling';
 import { Vpc, Instance, InstanceType, MachineImage } from '@aws-cdk/aws-ec2';
-import { CloudWatchAgent } from '../src';
+import { CloudWatchAgent, CloudWatchMetricBuilder } from '../src';
 
 const app = new App();
 const stack = new Stack(app, 'Stack', { env: { region: 'region' } });
@@ -23,12 +23,48 @@ new CloudWatchAgent(stack, 'CloudWatchAgent', {
 const synth = SynthUtils.toCloudFormation(stack);
 
 test('Cloud Watch Agent', () => {
+  const app = new App();
+  const stack = new Stack(app, 'Stack', { env: { region: 'region' } });
+  const instance = new Instance(stack, 'Instance', {
+    vpc: Vpc.fromVpcAttributes(stack, 'Vpc', {
+      vpcId: 'Vpc',
+      availabilityZones: ['a'],
+      isolatedSubnetIds: ['a-sub-1'],
+    }),
+    instanceType: new InstanceType('t3.nano'),
+    machineImage: MachineImage.latestAmazonLinux(),
+  });
+  const agent = new CloudWatchAgent(stack, 'CloudWatchAgent', {
+    compute: instance,
+    defaultMetrics: {
+      cpu: true,
+      memory: true,
+      disk: true,
+      network: true,
+    },
+  });
+
+  agent.addMetric(
+    new CloudWatchMetricBuilder('test')
+      .setNamespace('test_namespace')
+      .setInterval(30)
+      .addMeasurement('test_measurement')
+      .toMetric()
+  );
+
   const userData = stack.resolve(instance.userData.render());
   expect(userData).toContain('rpm');
   expect(userData).toContain('echo');
   expect(userData).toContain('amazon-cloudwatch-agent-ctl');
+
+  expect(userData).toContain('usage_nice');
   expect(userData).toContain('mem_used_percent');
   expect(userData).toContain('used_percent');
+  expect(userData).toContain('tcp_established');
+  expect(userData).toContain('udp_socket');
+  expect(userData).toContain('test_namespace');
+  expect(userData).toContain('test_measurement');
+
   expect(userData).toMatchSnapshot();
 });
 
