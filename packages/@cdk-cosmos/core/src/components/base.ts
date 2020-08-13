5568fd12780fd221ecef0d61fd5fbb9a9daf6db7
Timothy Purchas <timothy.purchas@iag.com.au>
2020-08-13 15:12:27 +1000

    Fix Nested Stack Naming Pattern

--- packages/@cdk-cosmos/core/src/components/base.ts
+++ packages/@cdk-cosmos/core/src/components/base.ts
@@ -105,6 +105,11 @@ export class BaseNestedStack extends NestedStack {
 
     this.node.type = type;
     this.hidden = new Construct(this, 'Default');
+
+    // Change Nested Stack Naming
+    this.nestedStackResource?.overrideLogicalId(id);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (this as any).templateFile = `${this.parentStack?.stackName}${id}.nested.template.json`;
   }
 
   protected allocateLogicalId(scope: CfnElement): string {
