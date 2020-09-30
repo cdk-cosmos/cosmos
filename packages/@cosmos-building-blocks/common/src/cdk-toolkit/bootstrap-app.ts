4a9f2bbed9567e64c1ee1169aff8454d7b330cf4
Timothy Purchas <timothy.purchas@iag.com.au>
2020-10-01 08:27:25 +1000

    Clean up bootstrap

--- packages/@cosmos-building-blocks/common/src/cdk-toolkit/bootstrap-app.ts
+++ packages/@cosmos-building-blocks/common/src/cdk-toolkit/bootstrap-app.ts
@@ -1,8 +1,8 @@
 import { App } from '@aws-cdk/core';
 import { CdkBootstrap } from './cdk-bootstrap';
 
-const deployCore = process.env.CDK_DEPLOY_CORE === 'true';
-const stacks = process.env.STACKS ?? (deployCore ? 'Core*' : 'App*');
+const deployCore = process.env.CORE === 'true';
+const stacks = process.env.STACKS || (deployCore ? 'Core*' : 'App*');
 
 const app = new App();
 
