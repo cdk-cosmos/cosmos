945071661e45c5ac66614cd21e1d7b4e29b64478
Tim P <timothy@purchas.com>
2020-11-24 09:38:04 +1100

    Fix Redis prop issue + Add is functions to extension classes (#220)

--- packages/@cdk-cosmos/core/src/galaxy/galaxy-extension-stack.ts
+++ packages/@cdk-cosmos/core/src/galaxy/galaxy-extension-stack.ts
@@ -1,8 +1,10 @@
-import { Construct, Tags } from '@aws-cdk/core';
+import { Construct, IConstruct, Tags } from '@aws-cdk/core';
 import { BaseStack, BaseStackProps } from '../components/base';
 import { ICosmosExtension } from '../cosmos/cosmos-extension-stack';
 import { IGalaxyCore } from './galaxy-core-stack';
 import { GalaxyCoreImportProps, GalaxyCoreImport } from './galaxy-core-import';
+
+const GALAXY_EXTENSION_SYMBOL = Symbol.for('@cdk-cosmos/core.GalaxyExtensionStack');
 
 export interface IGalaxyExtension extends Construct {
   cosmos: ICosmosExtension;
@@ -24,6 +26,8 @@ export class GalaxyExtensionStack extends BaseStack implements IGalaxyExtension 
       ...props,
     });
 
+    Object.defineProperty(this, GALAXY_EXTENSION_SYMBOL, { value: true });
+
     const { portalProps } = props || {};
 
     this.cosmos = cosmos;
@@ -33,5 +37,19 @@ export class GalaxyExtensionStack extends BaseStack implements IGalaxyExtension 
     });
 
     Tags.of(this).add('cosmos:galaxy:extension', this.node.id);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  static isGalaxyExtension(x: any): x is GalaxyExtensionStack {
+    return typeof x === 'object' && x !== null && GALAXY_EXTENSION_SYMBOL in x;
+  }
+
+  static of(construct: IConstruct): GalaxyExtensionStack {
+    const scopes = [construct, ...construct.node.scopes];
+    for (const scope of scopes) {
+      if (GalaxyExtensionStack.isGalaxyExtension(scope)) return scope;
+    }
+
+    throw new Error(`No Galaxy Core Stack could be identified for the construct at path ${construct.node.path}`);
   }
 }
