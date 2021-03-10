bd80614e20ea51cf6040500de64bd9761051cdfc
Timothy Purchas <timothy.purchas@iag.com.au>
2021-03-11 09:33:28 +1100

    Add test

---
+++ packages/@cosmos-building-blocks/common/test/state.test.ts
@@ -0,0 +1,22 @@
+import '@aws-cdk/assert/jest';
+import { SynthUtils } from '@aws-cdk/assert';
+import { App, Stack } from '@aws-cdk/core';
+import { SsmState } from '../src';
+
+const app = new App();
+const stack = new Stack(app, 'Stack');
+const state = new SsmState(stack, 'SsmState', {
+  name: 'SsmState',
+  value: 'new',
+});
+const state2 = new SsmState(stack, 'SsmState2', {
+  name: 'SsmState2',
+});
+
+const synth = SynthUtils.toCloudFormation(stack);
+
+test('SSM State', () => {
+  expect(state.value).toEqual('${Token[TOKEN.11]}');
+  expect(state2.value).toEqual('${Token[TOKEN.18]}');
+  expect(synth).toMatchSnapshot();
+});
