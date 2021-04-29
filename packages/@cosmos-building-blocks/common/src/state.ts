1a48af1d2009f8b2d536d13ac49e45d6e45fcc8b
Timothy Purchas <timothy.purchas@iag.com.au>
2021-04-30 09:30:54 +1000

    Clean up State Construct to use cfn param always

--- packages/@cosmos-building-blocks/common/src/state.ts
+++ packages/@cosmos-building-blocks/common/src/state.ts
@@ -15,32 +15,35 @@ export class SsmState extends Construct implements IState {
   readonly name: string;
   readonly value: string;
   readonly rawValue?: string;
-  readonly param: StringParameter;
+  readonly param: CfnParameter;
+  readonly state: StringParameter;
 
   constructor(scope: Construct, id: string, props: SsmStateProps) {
     super(scope, id);
 
     const { name, value } = props;
-    let _value = value;
 
-    if (_value) {
-      Annotations.of(this).addInfo(`Using '${_value}' value for ${name} state.`);
+    if (value) {
+      this.param = new CfnParameter(this, 'Param', {
+        type: 'String',
+        default: value,
+      });
+      Annotations.of(this).addInfo(`Using '${value}' value for ${name} state.`);
     } else {
-      const param = new CfnParameter(this, 'CurrentState', {
+      this.param = new CfnParameter(this, 'Param', {
         type: 'AWS::SSM::Parameter::Value<String>',
         default: name,
       });
-      _value = param.valueAsString;
       Annotations.of(this).addInfo(`Using Current value for ${name} state.`);
     }
 
-    this.param = new StringParameter(this, 'State', {
+    this.state = new StringParameter(this, 'State', {
       parameterName: name,
-      stringValue: _value,
+      stringValue: this.param.valueAsString,
     });
 
     this.name = name;
-    this.value = this.param.stringValue;
+    this.value = this.state.stringValue;
     this.rawValue = value;
   }
 }
